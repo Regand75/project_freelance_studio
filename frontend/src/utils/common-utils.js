b2cc8361136eb54937a0e47622f5633a78bfd03a
Regand75 <regandess@gmail.com>
2024-11-06 15:52:28 +0300

    page orders

--- frontend/src/utils/common-utils.js
+++ frontend/src/utils/common-utils.js
@@ -1,6 +1,7 @@
 import config from "../config/config";
 
 export class CommonUtils {
+
     static getLevelHtml(level) {
         let levelHtml = null;
         switch (level) {
@@ -17,5 +18,40 @@ export class CommonUtils {
                 levelHtml = `<span class="badge badge-secondary">Unknown</span>`;
         }
         return levelHtml;
+    }
+
+    static getStatusInfo(status) {
+        const info = {
+            name: '',
+            color: '',
+            icon: '',
+        };
+        switch (status) {
+            case config.orderStatuses.new:
+                info.name = 'Новый';
+                info.color = 'secondary';
+                info.icon = 'star';
+                break;
+            case config.orderStatuses.confirmed:
+                info.name = 'Подтвержден';
+                info.color = 'info';
+                info.icon = 'eye';
+                break;
+            case config.orderStatuses.success:
+                info.name = 'Выполнен';
+                info.color = 'success';
+                info.icon = 'check';
+                break;
+            case config.orderStatuses.canceled:
+                info.name = 'Отменен';
+                info.color = 'danger';
+                info.icon = 'times';
+                break;
+            default:
+                info.name = 'Неизвестно';
+                info.color = 'secondary';
+                info.icon = 'star';
+        }
+        return info;
     }
 }
